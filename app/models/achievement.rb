99b4a5b547d990c93e05dbfebc2ba31aa7571f19
BackIsBachus <backisbachus@backisbachus.ninja>
2016-01-11 01:42:37 +0100

    Annotate

--- app/models/achievement.rb
+++ app/models/achievement.rb
@@ -9,7 +9,6 @@
 #  game_id         :integer
 #  created_at      :datetime
 #  updated_at      :datetime
-#  icon_close_url  :string(255)
 #  icon_open_url   :string(255)
 #  icon_closed_url :string(255)
 #
